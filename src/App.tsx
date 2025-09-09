b479ff634fbd62add3086607bb8648e83d492697
nester1van <nester1van@yandex.ru>
2025-09-09 16:06:34 +0700

    feat: lazy loading for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { Suspense } from "react";
+import { Spin } from "antd";
 import { Route, Routes } from "react-router-dom";
 import { Layout } from "antd";
 import Header from "@/components/Header";
-import MainPage from "@/pages/MainPage";
-import CatalogPage from "@/pages/CatalogPage";
-import CartPage from "@/pages/CartPage";
 
 const { Content } = Layout;
+const MainPage = React.lazy(() => import('@/pages/MainPage'));
+const CatalogPage = React.lazy(() => import('@/pages/CatalogPage'));
+const CartPage = React.lazy(() => import('@/pages/CartPage'));  
 
 const App: React.FC = () => {
   return (
@@ -14,11 +15,13 @@ const App: React.FC = () => {
       <Header />
       <Content className="widget-catalog__content">
         <div className="widget-catalog__inner-content">
-          <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="/catalog" element={<CatalogPage />} />
-            <Route path="/cart" element={<CartPage />} />
-          </Routes>
+          <Suspense fallback={<Spin size="large" className="widget-catalog__full-page-spinner" />}>
+            <Routes>
+              <Route path="/" element={<MainPage />} />
+              <Route path="/catalog" element={<CatalogPage />} />
+              <Route path="/cart" element={<CartPage />} />
+            </Routes>
+          </Suspense>
         </div>
       </Content>
     </Layout>
